5b6a3fdd5e95abd8c315e3ec291df3ccc732a617
Sruthi <sruthiganesan11@gmail.com>
2024-05-02 20:57:22 +0530

    hospital backend with security working hospital ui started

--- HospitalManagementSystem/src/main/java/com/guvi/hospitalManagementSystem/repository/PatientRepo.java
+++ HospitalManagementSystem/src/main/java/com/guvi/hospitalManagementSystem/repository/PatientRepo.java
@@ -4,6 +4,7 @@ import com.guvi.hospitalManagementSystem.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository public interface PatientRepo extends JpaRepository<Patient, Long> {
+@Repository
+public interface PatientRepo extends JpaRepository<Patient, Long> {
 
 }
